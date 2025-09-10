8675cd55dd0cd93d44a59641c0d9562476f9b246
Mazhar Akhtar Ansari <mazharakhtar679@gmail.com>
2025-09-10 14:08:48 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import json
+import os
 from datetime import datetime
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
@@ -246,3 +247,4 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+
